a0e93cd75a563d3924d2a6bc10e32deff60501b8
James Clarke <jjjclarke@yahoo.com>
2025-10-02 11:38:51 +0100

    Basic lists working
    Signed-off-by: James Clarke <jjjclarke@yahoo.com>

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Simple task groups
+to_do = ['Task 1', 'Task 2', 'Task 3']
+doing = ['Task 1', 'Task 2', 'Task 3']
+done = ['Task 1', 'Task 2', 'Task 3']
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', to_do=to_do, doing=doing, done=done)
 
 if __name__ == ('__main__'):
     app.run(debug=True)
