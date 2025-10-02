d6f5ddf8704c021f5999bc45b82d36bf8b103025
James Clarke <jjjclarke@yahoo.com>
2025-10-02 12:09:32 +0100

    Unfinished checkbox & HTML formatting fix
    Signed-off-by: James Clarke <jjjclarke@yahoo.com>

--- app.py
+++ app.py
@@ -19,5 +19,15 @@ def add_task():
         to_do.append(new_task)
     return redirect(url_for('index'))
 
+@app.route('/move', methods=['POST'])
+def move_tasks():
+    move_tasks = request.form.getlist('taskCheckbox')
+    for index in map(int, move_tasks):
+        if 1 <= index <= len(to_do):
+            # TODO: Move task from to_do to doing
+            doing.append(to_do[index - 1])
+            to_do.remove(to_do[index - 1])
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
